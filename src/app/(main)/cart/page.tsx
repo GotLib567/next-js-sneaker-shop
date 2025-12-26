1e3d028b5cf613494ceff46967e842d85af46d7a
Широков Глеб <gotlibpro@yandex.ru>
2025-12-26 15:20:26 +0500

    features: NotFound page added

--- src/app/(main)/cart/page.tsx
+++ src/app/(main)/cart/page.tsx
@@ -3,6 +3,7 @@ import { useCartStore } from '@/src/features/cart/model/cart.store';
 import styles from './CartPage.module.css';
 import React, { useEffect } from 'react';
 import Link from 'next/link';
+import Breadcrumbs from '@/src/shared/ui/Breadcrumbs/Breadcrumbs';
 
 function CartPage() {
   const cartItems = useCartStore((state) => state.items);
@@ -11,25 +12,29 @@ function CartPage() {
     console.log('Cart items:', cartItems);
   }, [cartItems]);
 
-  if (cartItems.length === 0) {
-    return (
-      <div className={styles.emptyCart}>
-        <span className={styles.title}>В корзине пока нет товаров</span>
-        <Link href="/catalog" className={styles.catalogBtn}>Перейти в каталог</Link>
-      </div>
-    )
-  }
-
   return (
     <div className={styles.root}>
-      <div>Товары: {cartItems.length}</div>
-      <ul>
-        {cartItems.map((item, index) => (
-          <li key={`${item.id}-${item.size}-${index}`}>
-            Товар {item.id}, размер {item.size}
-          </li>
-        ))}
-      </ul>
+      <Breadcrumbs items={[
+        { label: "Главная", href: "/" },
+        { label: "Корзина" },
+      ]} />
+      {cartItems.length !== 0 ? (
+        <div>
+          <div>Товары: {cartItems.length}</div>
+          <ul>
+            {cartItems.map((item, index) => (
+              <li key={`${item.id}-${item.size}-${index}`}>
+                Товар {item.id}, размер {item.size}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div className={styles.emptyCart}>
+          <span className={styles.title}>В корзине пока нет товаров</span>
+          <Link href="/catalog" className={styles.catalogBtn}>Перейти в каталог</Link>
+        </div>
+      )}
     </div>
   )
 }
---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import styles from "./NotFound.module.css"
+
+function NotFound() {
+  return (
+    <div className={styles.root}>
+      Извините, страница не найдена :(
+    </div>
+  )
+}
+
+export default NotFound;
